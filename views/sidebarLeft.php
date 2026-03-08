79fbfd507464a980f7360df6e02f112f73c81bf2
Sven Berger <bergersvenoliver@gmail.com>
2026-03-08 12:14:04 +0100

    clear urls

--- views/sidebarLeft.php
+++ views/sidebarLeft.php
@@ -1,8 +1,8 @@
 <div class="sidebar-box rounded-5 p-lg-5 p-md-4 p-4 text-white mb-3">
     <nav class="sidebar-nav">
         <ul class="list-unstyled d-grid gap-3 gap-md-5 gap-lg-3 mb-0">
-            <li><a href="ruben-winkler.php?js=hello">Hallo JavaScript</a></li>
-            <li><a href="ruben-winkler.php?php=hello">Hallo PHP</a></li>
+            <li><a href="ruben-winkler.php?js=hello" class="text-black text-decoration-none">Hallo JavaScript</a></li>
+            <li><a href="ruben-winkler.php?php=hello" class="text-black text-decoration-none">Hallo PHP</a></li>
         </ul>
     </nav>
 </div>
