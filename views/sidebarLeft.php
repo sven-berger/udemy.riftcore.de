c9f3ee084416b313f82d49d790b451c5edc2c7f0
Sven Berger <bergersvenoliver@gmail.com>
2026-03-08 12:31:04 +0100

    Test for Deploy

--- views/sidebarLeft.php
+++ views/sidebarLeft.php
@@ -1,8 +1,11 @@
 <div class="sidebar-box rounded-5 p-lg-5 p-md-4 p-4 text-white mb-3">
     <nav class="sidebar-nav">
         <ul class="list-unstyled d-grid gap-3 gap-md-5 gap-lg-3 mb-0">
-            <li><a href="ruben-winkler.php?js=hello" class="text-success text-decoration-none">Hallo JavaScript</a></li>
-            <li><a href="ruben-winkler.php?php=hello" class="text-success text-decoration-none">Hallo PHP</a></li>
+            <li><a href="ruben-winkler.php?js=hello" class="text-success text-decoration-none">Hallo JavaScript bei
+                    Ruben Winkler</a></li>
+            <li><a href="ruben-winkler.php?php=hello" class="text-success text-decoration-none">Hallo PHP bei Ruben
+                    Winkler</a></li>
+
         </ul>
     </nav>
 </div>
