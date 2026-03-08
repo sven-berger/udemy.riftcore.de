23838c2fdaaadd962d6274b9ab004248e4652d09
Sven Berger <bergersvenoliver@gmail.com>
2026-03-08 16:06:25 +0100

    fix navbar mobile

--- views/navbar.php
+++ views/navbar.php
@@ -1,45 +1,53 @@
 <nav class="m-3 rounded-5 navbar navbar-expand-lg navbar-dark navbar-header-theme p-3">
     <div class="container-fluid">
-        <div class="grid gap-2 gap-md-2 gap-lg-2">
-            <p class="m-0"><a class="navbar-brand text-white" href="index.php">udemy | RiftCore.de</a></p>
-            <p class="m-0"><small class="text-secondary text-light">Meine Präsentation am erlernten Wissen durch
-                    meine
-                    Kurse bei
-                    udemy</small></p>
-        </div>
-        <button class="navbar-toggler border-light" type="button" data-bs-toggle="collapse"
+        <button class="navbar-toggler bg-warning border-warning" type="button" data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
             aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse flex justify-content-end navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Dropdown
-                    </a>
+        <div class="col-12 col-lg-4">
+            <p class="mb-0 mt-4 mt-lg-0"><a class="navbar-brand text-white" href="/">udemy | RiftCore.de</a>
+            </p>
+            <p class="m-0"><small class="text-secondary text-light">Meine Praesentation am erlernten Wissen
+                    durch meine Kurse bei udemy</small></p>
+        </div>
 
-                    <ul class="dropdown-menu navbar-dropdown-theme">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
+        <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
+
+            <div class="row w-100 g-3 align-items-center mt-2 mt-lg-0">
+
+                <div class="col-12 col-lg-4 d-flex justify-content-lg-center">
+                    <ul class="navbar-nav flex-row gap-2 gap-lg-3 mb-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Startseite</a>
                         </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu navbar-dropdown-theme">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-light" type="submit">Search</button>
-            </form>
+                </div>
+
+                <div class="col-12 col-lg-4 d-flex justify-content-lg-end">
+                    <form class="d-flex w-45 navbar-search-form" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-light" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </nav>
